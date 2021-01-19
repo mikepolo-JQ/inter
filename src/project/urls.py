e0965e4684ee855143231bad2670793a52f3898c
Misha Polozkov <polozkov1450@gmail.com>
2021-01-19 20:23:51 +0300

    add database and onboarding app

--- src/project/urls.py
+++ src/project/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
-
 urlpatterns = [
     path("admin/", admin.site.urls, name="admin"),
     path("", include("applications.main.urls"), name="index"),
+    path("o/", include("applications.onboarding.urls"), name="onboarding"),
+
 ]
