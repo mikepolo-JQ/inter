41bcef6771244ced938577042339f003b6f69740
Misha Polozkov <polozkov1450@gmail.com>
2021-02-25 18:01:50 +0300

    getReason => getProfilePageStyle ( view active HS input )

--- src/applications/profile/static/profile/js/profile.js
+++ src/applications/profile/static/profile/js/profile.js
@@ -14,7 +14,54 @@ openFeedbackBox = function (){
     window.scrollTo(0, offsetY);
 }
 
-const getReason = function (pk){
+
+plusInput = function (element){
+
+    element.style.display = "none";
+
+    let topValue, doActiveId, classToMove;
+
+    if(element.id.startsWith("need")) {
+        if (element.id.startsWith("need_first")) {
+            topValue = -27;
+            doActiveId = "second_input_need";
+            classToMove = "input_move";
+        } else {
+            doActiveId = "third_input_need";
+            classToMove = "input_move";
+        }
+    }else{
+        if (element.id.startsWith("provide_first")) {
+            doActiveId = "second_input_provide";
+            classToMove = "input_move_provide";
+        } else {
+            doActiveId = "third_input_provide";
+            classToMove = "input_move_provide";
+        }
+    }
+
+    function f() {
+        document.getElementById(doActiveId).classList.toggle("active_input");
+    }
+
+    let moveElements = document.getElementsByClassName(classToMove)
+
+
+    console.log(parseInt(moveElements[0].style.top) - 10)
+    for(let i = 0; i < moveElements.length; ++i){
+        let elementTopValue = parseInt(moveElements[i].style.top)
+        if (elementTopValue){
+            elementTopValue += 27;
+        }else { elementTopValue = topValue}
+        moveElements[i].style.top = elementTopValue + "px";
+    }
+
+    setTimeout(f, 600);
+}
+
+// function to get profile image style,
+// reasons of contacts and view active input
+const getProfilePageStyles = function (pk){
 
     let api_url = "/profile/reasons_and_back/1/"
 
@@ -27,11 +74,26 @@ const getReason = function (pk){
         resp => {
             resp.json().then(
                 resp_payload => {
+                    //view active input
+                    let noActiveInputList = [
+                        ["second_input_need_help", "need_first_plus"],
+                        ["third_input_need_help", "need_second_plus"],
+                        ["second_input_provide_help", "provide_first_plus"],
+                        ["third_input_provide_help" , "provide_second_plus"],
+                    ]
+
+                    for(let [key, value] of noActiveInputList){
+                        let noActiveInput = document.getElementById(key);
+                        if(noActiveInput && noActiveInput.value){
+                            document.getElementById(value).click()
+                        }
+                    }
+
+                    // get profile avatar styles and reasons of contacts
                     if (resp_payload.ok) {
                         let contacts_pk = resp_payload.contacts_pk;
                         let contact_reasons = resp_payload.contact_reasons;
                         let contact_background = resp_payload.contact_background;
-
 
                         if(pk){
                         document.getElementById("avatar_" + pk).style.borderColor = resp_payload.color_to_pk;}
